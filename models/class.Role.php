1262df6b45c1788e512c337803e8f4c8020aa556
Thomas Vathonne <2249740+zlezero@users.noreply.github.com>
2020-06-17 19:31:22 +0200

    feat(models): ajout des classes CB / Role / User

---
+++ models/class.Role.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once("Model.php");
+
+class Role {
+
+    private int $_idRole;
+    private string $_nomRole;
+
+    private $bdd;
+
+    public function __construct(int $idRole) {
+
+        try {
+        
+            $this->bdd = Model::getDatabase();
+        
+            $req = $this->bdd->prepare("SELECT * FROM role WHERE idRole = :id");
+            $req->bindParam('id', $idRole);
+    
+            $req->execute();
+            
+            $data = $req->fetch();
+    
+            if (count($data) > 0) {        
+                $this->_idUtilisateur = $idRole;
+                $this->_nomRole = $data["nomRole"];     
+            } else {
+                throw new Exception("Aucun rôle n'existe avec cet identifiant");
+            }
+
+        } catch (Exception $e) {
+            throw new Exception($e);
+        }
+
+    }
+
+    public function getIdRole() {
+        return $this->_idRole;
+    }
+
+    public function getNomRole() {
+        return $this->_nomRole;
+    }
+    
+    public function setNomRole(string $nomRole) {
+        $this->_nomRole = $nomRole;
+    }
+
+}
+
+?>
---
+++ models/class.User.php
@@ -0,0 +1,96 @@
+<?php
+
+require_once("Model.php");
+require_once("class.Role.php");
+require_once("class.CB.php");
+
+class User {
+
+    private int $_idUtilisateur;
+    private string $_nom;
+    private string $_prenom;
+    private string $_email;
+    private Role $_role;
+    private CB $_CB;
+
+    private $bdd;
+
+    public function __construct(int $idUser) {
+
+        try {
+        
+            $this->bdd = Model::getDatabase();
+        
+            $req = $this->bdd->prepare("SELECT * FROM utilisateur WHERE idUtilisateur = :id");
+            $req->bindParam('id', $idUser);
+    
+            $req->execute();
+            
+            $data = $req->fetch();
+    
+            if (count($data) > 0) {
+
+                $this->_idUtilisateur = $idUser;
+                $this->_nom = $data["nom"];
+                $this->_prenom = $data["prenom"];
+                $this->_email = $data["email"];
+                $this->_role = new Role($data["idRole"]);
+                $this->_idCB = new CB($data["idCB"]);
+        
+            } else {
+                throw new Exception("Aucun utilisateur n'existe avec cet identifiant");
+            }
+
+        } catch (Exception $e) {
+            throw new Exception($e);
+        }
+
+    }
+
+    public function getIdUtilisateur() {
+        return $this->_idUtilisateur;
+    }
+
+    public function getNom() {
+        return $this->_nom;
+    }
+
+    public function getPrenom() {
+        return $this->_prenom;
+    }
+
+    public function getEmail() {
+        return $this->_email;
+    }
+
+    public function getRole() {
+        return $this->_role;
+    }
+
+    public function getCB() {
+        return $this->_CB;
+    }
+
+    public function setNom(string $nom) {
+        $this->_nom = $nom;
+    }
+
+    public function setPrenom(string $prenom) {
+        $this->_prenom = $prenom;
+    }
+
+    public function setEmail(string $email) {
+        $this->_email = $email;
+    }
+
+    public function setRole(Role $role) {
+        $this->_role = $role;
+    }
+
+    public function setCB(CB $CB) {
+        $this->_CB = $CB;
+    }
+    
+}
+
+?>
