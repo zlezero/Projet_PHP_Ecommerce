f14a6d310499458e93be9a2456388c4ea0911aee
Aminata <aminatacorrea@yahoo.fr>
2020-06-20 21:59:47 +0200

    Modify some classes and views

--- views/include/view_header.php
+++ views/include/view_header.php
@@ -44,7 +44,7 @@
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div class="container">
 
-                <a class="navbar-brand" href="#">VintAIR</a>
+                <a class="navbar-brand" href="index.php?controller=articles#"><img alt="Logo" src="content/images/vintairLogoFull.png" width="32%"></a>
 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -53,7 +53,7 @@
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="nav navbar-nav">
                         <li class="nav-item active">
-                            <a class="nav-link" href="#">Accueil
+                            <a class="nav-link" href="index.php?controller=articles#">Accueil
                                 <span class="sr-only">(current)</span>
                             </a>
                         </li>
@@ -83,7 +83,7 @@
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div class="container">
 
-                <a class="navbar-brand" href="index.php">VintAIR</a>
+                <a class="navbar-brand" href="index.php"><img alt="Logo" src="content/images/vintairLogoFull.png" width="32%"></a>
 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -96,26 +96,23 @@
                                 <span class="sr-only">(current)</span>
                             </a>
                         </li>
+                        <li class="nav-item order-2 order-md-1"><a href="index.php?controller=commande&action=liste" class="nav-link" title="Historique des commandes">Commandes</a></li>
                     </ul>
                     <ul class="nav navbar-nav flex-row justify-content-between ml-auto">
                         <!--TODO : critère si l'utilisateur est connecté TO DO -->
-                        <li class="nav-item order-2 order-md-1"><a href="index.php?controller=commande&action=liste" class="nav-link" title="Historique des commandes">Commandes</a></li>
                         <li class="nav-item order-2 order-md-1"><a href="index.php?controller=commande&action=consulter" class="nav-link" title="Panier"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a></li>
 
-                        
 
-                        <li class="nav-item order-2 order-md-1"><a href="#" class="nav-link" title="settings"><i class="fa fa-cog fa-fw fa-lg"></i></a></li>
                         <li class="dropdown order-1">
-
 
                             <?php if ($this->getSessionManager()->isConnected()) { ?>
 
                                 <button type="button" id="dropdownMenu1" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Mon compte <span class="caret"></span></button>
-                                <ul class="dropdown-menu dropdown-menu-right mt-2">
-                                    <li class="px-3 py-2">
-                                        <?php echo("Bonjour ". $this->getSessionManager()->getUser()->getPrenom() . " " . $this->getSessionManager()->getUser()->getNom()); ?>
+                                <ul class="dropdown-menu dropdown-menu-right mt-2 justify-content-between">
+                                    <li class="px-3 py-1">
+                                        <?php echo("<div align='center'> Bonjour</div><div align='center'>". $this->getSessionManager()->getUser()->getPrenom() . " " . $this->getSessionManager()->getUser()->getNom().'</div>'); ?>
                                         <div class="form-group">
-                                            <button type="submit" class="btn btn-primary btn-block" id="disconnectButton">Se déconnecter</button>
+                                            <button type="submit" class="btn btn-primary btn-block btn-sm" id="disconnectButton">Se déconnecter</button>
                                         </div>
                                     </li>
                                 </ul>
@@ -135,6 +132,7 @@
                                             <div class="form-group">
                                                 <button type="submit" class="btn btn-primary btn-block">Se connecter</button>
                                             </div>
+                                            <div class="dropdown-divider"></div>
                                             <div class="form-group text-center">
                                                 <small><a href="#" data-toggle="modal" data-target="#modalPassword">Pas de compte ? S'inscrire</a></small>
                                             </div>
