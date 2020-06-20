0f5f70722fe11abf105bde9f3a5111431e647953
Thomas Vathonne <2249740+zlezero@users.noreply.github.com>
2020-06-20 18:50:03 +0200

    fix(admin): ajout des checks admin

--- controllers/Controller_articles.php
+++ controllers/Controller_articles.php
@@ -12,7 +12,9 @@ class Controller_articles extends Controller {
     }
 
     public function action_admin(){
+
         if ($this->checkAdmin()) { 
+
             $articlesManager = new ArticlesManager();
             $categoriesManager = new CategorieManager();
             $configOrder = new ConfigArticles();
@@ -24,121 +26,165 @@ class Controller_articles extends Controller {
                 'categories'=> $categories,
                 'typeAffichage'=> $defaultValue
             ]);
+
+        } else {
+            redirect("index.php");
         }
     }
 
     public function action_defineDefaultOrder(){
         
         if ($this->checkAdmin()) {
+
             $defaultOrd = $_GET['value'] ?? false;
             if($defaultOrd){
                 $config = new ConfigArticles();
                 $config->updateDefaultOrder($defaultOrd);
             }
 
+            echo json_encode($defaultOrd);
+
+        } else {
+            echo json_encode("NOK");
         }
-        echo json_encode($defaultOrd);
+
     }
 
     public function action_fetchArticle(){
 
-        $article = false;
-        if(!empty($_GET)){
-            $articlesOfManager = new ArticlesManager();
-            $idArticle = $_GET['value'] ?? false;
-            if($idArticle){
-                if($articlesOfManager->checkArticleExists($idArticle)){
-                    $article = $articlesOfManager->getArticle($idArticle);
-                } 
+        if ($this->checkAdmin()) {
+
+            $article = false;
+
+            if(!empty($_GET)) {
+                $articlesOfManager = new ArticlesManager();
+                $idArticle = $_GET['value'] ?? false;
+                if($idArticle){
+                    if($articlesOfManager->checkArticleExists($idArticle)){
+                        $article = $articlesOfManager->getArticle($idArticle);
+                    } 
+                }
             }
+
+            echo json_encode($article);
+
+        } else {
+            echo json_encode("NOK");
         }
-        echo json_encode($article);
 
     }
 
     public function action_addArticle(){
 
-        #TO DO Check si admin est connecté
-        if(!empty($_POST)){
-            
-            $articlesOfManager = new ArticlesManager();
+        if ($this->checkAdmin()) {
 
-            $articleName = $_POST['nomArticle'] ?? false;
-            $articleDescription = $_POST['descriptionArticle'] ?? false;
-            $articleUrl = $_POST['urlPhoto'] ?? false;
-            $articlePrice = $_POST['prix'] ?? false;
-            $articleQuantity = $_POST['quantite'] ?? false;
-            $idCategorie = $_POST['idCategorie'] ?? false;
-            $articleCategorie = new Categorie(intval($idCategorie));
-            
-            if($articleName && $articleDescription && $articleUrl && $articlePrice && $articleQuantity && $articleCategorie){
-                if(is_numeric($articlePrice) && is_numeric($articleQuantity)){
-                    $articlesOfManager->addArticle($articleName,$articleDescription,$articleUrl,floatval($articlePrice),intval($articleQuantity),$articleCategorie);
-                    $message = "L'article a bien été ajouté.";
-                } else{
-                    $message = "La quantité et le prix doivent être positifs.";
+            if(!empty($_POST)) {
+
+                $articlesOfManager = new ArticlesManager();
+    
+                $articleName = $_POST['nomArticle'] ?? false;
+                $articleDescription = $_POST['descriptionArticle'] ?? false;
+                $articleUrl = $_POST['urlPhoto'] ?? false;
+                $articlePrice = $_POST['prix'] ?? false;
+                $articleQuantity = $_POST['quantite'] ?? false;
+                $idCategorie = $_POST['idCategorie'] ?? false;
+                $articleCategorie = new Categorie(intval($idCategorie));
+                
+                if($articleName && $articleDescription && $articleUrl && $articlePrice && $articleQuantity && $articleCategorie){
+                    if(is_numeric($articlePrice) && is_numeric($articleQuantity)){
+                        $articlesOfManager->addArticle($articleName,$articleDescription,$articleUrl,floatval($articlePrice),intval($articleQuantity),$articleCategorie);
+                        $message = "L'article a bien été ajouté.";
+                    } else {
+                        $message = "La quantité et le prix doivent être positifs.";
+                    }
+                } else {
+                    $message = "Tous les champs ne sont pas remplis.";
                 }
-            } else{
-                $message = "Tous les champs ne sont pas remplis.";
+
+            } else {
+                $message = "Arguments invalides";
             }
+
+        } else {
+            $message = "NOK";
         }
+
         echo json_encode($message);
+
     }
 
     public function action_updateArticle(){
         
-        #TO DO Check si admin est connecté
-        if(!empty($_POST)){
-            $articlesOfManager = new ArticlesManager();
+        if ($this->checkAdmin()) {
 
-            $idArticle = $_POST['idArticle'] ?? false;
-            $newName = $_POST['nomArticle'] ?? false;
-            $newDescription = $_POST['descriptionArticle'] ?? false;
-            $newUrl = $_POST['urlPhoto'] ?? false;
-            $newPrice = $_POST['prix'] ?? false;
-            $newQuantity = $_POST['quantite'] ?? false;
-            $newIdCategorie = $_POST['idCategorie'] ?? false;
-            $newArticleCategorie = new Categorie(intval($newIdCategorie));
+            if(!empty($_POST)) {
 
-            if($idArticle && $newName && $newDescription && $newUrl && $newPrice && $newQuantity && $newArticleCategorie){
-                $article = new Article($idArticle);
-                $article->setNomArticle($newName);
-                $article->setDescriptionArticle($newDescription);
-                $article->setUrlPhotoArticle($newUrl);
-                $article->setPrixArticle($newPrice);
-                $article->setQuantiteArticle($newQuantity);
-                $article->setCategorieArticle($newArticleCategorie);
-                $articlesOfManager->updateArticle($article);
-                $message="La mise à jour de l'article s'est bien passé.";
-            } else{
-                $message = "Tous les champs ne sont pas remplis.";
+                $articlesOfManager = new ArticlesManager();
+    
+                $idArticle = $_POST['idArticle'] ?? false;
+                $newName = $_POST['nomArticle'] ?? false;
+                $newDescription = $_POST['descriptionArticle'] ?? false;
+                $newUrl = $_POST['urlPhoto'] ?? false;
+                $newPrice = $_POST['prix'] ?? false;
+                $newQuantity = $_POST['quantite'] ?? false;
+                $newIdCategorie = $_POST['idCategorie'] ?? false;
+                $newArticleCategorie = new Categorie(intval($newIdCategorie));
+    
+                if($idArticle && $newName && $newDescription && $newUrl && $newPrice && $newQuantity && $newArticleCategorie){
+                    $article = new Article($idArticle);
+                    $article->setNomArticle($newName);
+                    $article->setDescriptionArticle($newDescription);
+                    $article->setUrlPhotoArticle($newUrl);
+                    $article->setPrixArticle($newPrice);
+                    $article->setQuantiteArticle($newQuantity);
+                    $article->setCategorieArticle($newArticleCategorie);
+                    $articlesOfManager->updateArticle($article);
+                    $message="La mise à jour de l'article s'est bien passé.";
+                } else {
+                    $message = "Tous les champs ne sont pas remplis.";
+                }
+            } else {
+                $message = "Arguments invalides";
             }
+
+        } else {
+            $message = "NOK";
         }
+
         echo json_encode($message);
 
     }
 
 	public function action_deleteArticle() {
 
-        #TO DO Check si admin est connecté
-        if(!empty($_GET)){
-            $articlesOfManager = new ArticlesManager();
+        if ($this->checkAdmin()) {
 
-            $idArticle = $_GET['value'] ?? false;
-
-            if($idArticle){
-                if($articlesOfManager->checkArticleExists($idArticle)){
-                    #TO DO check if article not in commande
-                    $articlesOfManager->deleteArticle($idArticle);
-                    $message= "L'article a bien été supprimé.";
-                } else{
-                    $message = "L'article que vous désirez supprimer n'existe pas.";
+            if(!empty($_GET)){
+                $articlesOfManager = new ArticlesManager();
+    
+                $idArticle = $_GET['value'] ?? false;
+    
+                if($idArticle){
+                    if($articlesOfManager->checkArticleExists($idArticle)){
+                        #TO DO check if article not in commande
+                        $articlesOfManager->deleteArticle($idArticle);
+                        $message= "L'article a bien été supprimé.";
+                    } else{
+                        $message = "L'article que vous désirez supprimer n'existe pas.";
+                    }
+                }else{
+                    $message = "Fournissez l'id de l'article que vous désirez supprimer.";
                 }
-            }else{
-                $message = "Fournissez l'id de l'article que vous désirez supprimer.";
+            } else {
+                $message = "Arguments invalides";
             }
+
+        } else {
+            $message = "NOK";
         }
+
         echo json_encode($message);
+
     }
 
     private function checkAdmin() {
--- views/include/view_footer.php
+++ views/include/view_footer.php
@@ -1,6 +1,6 @@
     <footer class="py-5 bg-dark fixed-bottom" style="margin-top: 15px">
         <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+            <p class="m-0 text-center text-white">Copyright &copy; VintAIR 2020</p>
         </div>
     </footer>
 
