4920c7815ecf701002d4b5e18a6c136bd8cc1e7a
Thomas Vathonne <2249740+zlezero@users.noreply.github.com>
2020-06-21 19:23:38 +0200

    fix(général): fixs

--- models/class.ArticlesManager.php
+++ models/class.ArticlesManager.php
@@ -136,15 +136,25 @@ class ArticlesManager{
             $start = $offset + 1;
             $end = min(($offset + $limit), $total);
 
-            $this->prevlink = ($this->page > 1) ? '<a href="?page=1" title="First page">&laquo;</a> <a href="?page=' . ($this->page - 1) . '" title="Previous page">&lsaquo;</a>' : '<span class="disabled">&laquo;</span> <span class="disabled">&lsaquo;</span>';
-            $this->nextlink = ($this->page < $this->pages) ? '<a href="?page=' . ($this->page + 1) . '" title="Next page">&nbsp;&rsaquo;</a> <a href="?page=' . $this->pages . '" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
+            if($this->page > 0) {
+                $offset = ($this->page - 1)  * $limit;
+                $start = $offset + 1;
+                $end = min(($offset + $limit), $total);
+            } else {
+                $offset = 0;
+                $start = $offset + 1;
+                $end = min(($offset + $limit), $total);
+            }
+
+            $this->prevlink = ($this->page > 1) ? '<a class="page-link" href="?page=1" title="First page">&laquo;</a></li> <li class="page-item"><a class="page-link" href="?page=' . ($this->page - 1) . '" title="Previous page">&lsaquo;</a>' : '<a class="page-link" href="#" aria-label="Previous"><span class="disabled">&laquo;</span></a></li> <li class="page-item"><a class="page-link" href="#" aria-label="First Page"><span class="disabled">&lsaquo;</span></a>';
+            $this->nextlink = ($this->page < $this->pages) ? '<a class="page-link" href="?page=' . ($this->page + 1) . '" title="Next page">&rsaquo;</a></li> <li class="page-item"><a class="page-link" href="?page=' . $this->pages . '" title="Last page">&raquo;</a>' : '<a class="page-link" href="#" aria-label="Next"><span class="disabled">&rsaquo;</span></a></li> <li class="page-item"><a class="page-link" href="#" aria-label="Last Page"><span class="disabled">&raquo;</span></a>';
         
             $stmt = $this->bdd->prepare('
                 SELECT *  
                 FROM article
                 ORDER BY  idArticle
                 LIMIT :limit
-                OFFSET :offset         
+                OFFSET :offset
             ');
             
             $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
