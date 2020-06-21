a45c027c06a03035364c7f8254452035d312bfbd
Thomas Vathonne <2249740+zlezero@users.noreply.github.com>
2020-06-21 21:26:21 +0200

    fix(payementCB): fix du module

--- models/class.CBManager.php
+++ models/class.CBManager.php
@@ -12,10 +12,18 @@ class CBManager {
 
     public function addCB(string $numCB, string $nomCompletCB, DateTime $dateExpirationCB, int $cryptoCB) {
         
-        $sql = "INSERT INTO CB(numCB, dateExpirationCB, cryptoCB, nomCompletCB) VALUES(:nom, :prenom, :email, :mdp, :idRole, :idCB)";
+        $sql = "INSERT INTO CB(numCB, dateExpirationCB, nomCompletCB, cryptoCB) VALUES(:numCB, :dateExpirationCB, :nomCompletCB, :cryptoCB)";
 
-        
+        $req = $this->bdd->prepare($sql);
 
+        $req->bindValue('numCB', $numCB);
+        $req->bindValue('dateExpirationCB', $dateExpirationCB->format("Y-m-d"));
+        $req->bindValue('cryptoCB', $cryptoCB);
+        $req->bindValue('nomCompletCB', $nomCompletCB);
+
+        $req->execute();
+
+        return new CB((intVal($this->bdd->lastInsertId())));
 
     }
 }
